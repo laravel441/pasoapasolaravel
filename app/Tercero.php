4f5af1587a1f483f8802b229aa22bc7e65f5139f
Ricardo Uricoechea <pruebas.eclipse@gmail.com>
2015-04-21 19:18:20 -0500

    jum

--- app/Tercero.php
+++ app/Tercero.php
@@ -1,9 +1,8 @@
 <?php namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Tercero extends Model {
-
 	protected $table = 'terceros';
-
+	protected $fillable = array (
+		'nit', 'nombre', 'rol', 'direccion', 'telefono', 'email', 'notas'
+	      );
 }
--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -9,7 +9,7 @@
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
-							<strong>Whoops!</strong> Ocurrio un problema con tu ingreso.<br><br>
+							<strong>Ooops!</strong> Ocurrio un problema con tu ingreso.<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
 									<li>{{ $error }}</li>
