fd3c8f841e34320d44a5ff31092a5b1b32d7cfa8
Ricardo Uricoehea <pruebas.eclipse@gmail.com>
2015-07-28 16:11:53 -0500

    28-08-15 16:11

--- resources/views/admin/roles/crearRol.blade.php
+++ resources/views/admin/roles/crearRol.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="row">
-        <div class="col-md-6 col-md-offset-3">
+        <div class="col-md-7 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Crear Roles</div>
 
